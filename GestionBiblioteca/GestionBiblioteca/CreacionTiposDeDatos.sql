cdc580c6bcde3c8286891cbe41bbd8cd138ebeb0
MarianaGallego <marianagallego97@hotmail.com>
2025-08-08 23:17:05 -0500

    [CreacionTiposDeDatosFinalizado]

---
+++ GestionBiblioteca/GestionBiblioteca/CreacionTiposDeDatos.sql
@@ -0,0 +1,8 @@
+------> Crear tipo de dato idNumero
+CREATE TYPE idNumero FROM INT NOT NULL
+
+------> Crear tipo de dato abreviatura
+CREATE TYPE abreviatura FROM CHAR(3) NOT NULL
+
+------> Crear tipo de dato descripcion
+CREATE TYPE descripcion FROM VARCHAR(100)
